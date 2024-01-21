cda847b77ab92d8b9a01cd21382128b7a7045e82
JurabekAminjonov <jurabekaminjonov3@gmail.com>
2024-01-21 21:24:21 +0300

    qayta tahrir

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-const savollar = {
-    savol:"kim qachon o'lgan",
-    vareant:"qachondir",
-    vareant1:"kim qachondir",
-    vareant2:"kelasi zamon",
-    javob:"savol xato"
-}
+let Array = [
+    {
+        savol: "Dasturiy ta'minot inqirozining asosiy sababi nima?",
+        javob:"hisoblash quvvatining keskin oshishi",
+        njavob1: "dasturlashning keskin o'sishi",
+        njavob2: "kompyuterlar narxining keskin oshishi",
+        njavob3:"xarajatlarni kamaytirish kompyuterlar"
+    },
+    {
+        savol: "Dasturiy ta'minot kompaniyasi tomonidan yaratilgan va ochiq dasturiy ta'minot bozorida sotiladigan mustaqil dasturiy mahsulotlarning nomlari qanday? ",
+        javob: "umumiy dasturiy mahsulotlar haqida",
+        njavob1: "maxsus dasturiy mahsulotlar ",
+        njavob2: "OS ",
+        njavob3: "Axborot tizimlari ",
+    },
+    {
+        savol:" Qaysi modelda prototiplar yaratilgan - yaratilayotgan dasturiy mahsulotning qisman funksionalligini amalga oshiradigan dasturlar ?",
+        javob: "Spiral model",
+        njavob1: "Iterativ model",
+        njavob2: "Evolyutsion yondashuv",
+        njavob3: "Kaskad yondashuvi"
+    },
+    {
+        savol: "1Dasturiy ta'minot inqirozining asosiy sababi nima?",
+        javob:"hisoblash quvvatining keskin oshishi",
+        njavob1: "dasturlashning keskin o'sishi",
+        njavob2: "kompyuterlar narxining keskin oshishi",
+        njavob3:"xarajatlarni kamaytirish kompyuterlar"
+    },
+    {
+        savol: "1Dasturiy ta'minot kompaniyasi tomonidan yaratilgan va ochiq dasturiy ta'minot bozorida sotiladigan mustaqil dasturiy mahsulotlarning nomlari qanday? ",
+        javob: "umumiy dasturiy mahsulotlar haqida",
+        njavob1: "maxsus dasturiy mahsulotlar ",
+        njavob2: "OS ",
+        njavob3: "Axborot tizimlari ",
+    },
+    {
+        savol:"1 Qaysi modelda prototiplar yaratilgan - yaratilayotgan dasturiy mahsulotning qisman funksionalligini amalga oshiradigan dasturlar ?",
+        javob: "Spiral model",
+        njavob1: "Iterativ model",
+        njavob2: "Evolyutsion yondashuv",
+        njavob3: "Kaskad yondashuvi"
+    }
+    
+]
+
+let savolObj = Array[Math.floor(Math.random()*4)]
 
 
-
+console.log(savolObj);
 
 let uniqueNumbers = [];
 for (let i = 0; i < 4; i++) {
@@ -31,20 +71,20 @@ let vareantlar2 = document.getElementsByClassName('javoblar-item')[b];
 let vareantlar3 = document.getElementsByClassName('javoblar-item')[c];
 let vareantlar4 = document.getElementsByClassName('javoblar-item')[d];
 
-savol.innerHTML = savollar.savol;
-vareantlar1.innerHTML = savollar.vareant
-vareantlar2.innerHTML = savollar.vareant1
-vareantlar3.innerHTML = savollar.vareant2
-vareantlar4.innerHTML =savollar.javob
-let button = document.querySelectorAll('button');
 
-button.forEach((butoon) => {
-    butoon.addEventListener('click', ()=>{
-        if(butoon.innerText == savollar.javob){
-            butoon.style.backgroundColor = 'rgb(50, 206, 19)'
+savol.innerHTML = savolObj.savol;
+vareantlar1.innerHTML = savolObj.javob;
+vareantlar2.innerHTML = savolObj.njavob1;
+vareantlar3.innerHTML = savolObj.njavob2;
+vareantlar4.innerHTML = savolObj.njavob3;
+
+let button = document.querySelectorAll('button');
+button.forEach((button)=>{
+    button.addEventListener('click', ()=>{
+        if(button.innerText == savolObj.javob){
+            button.style.backgroundColor = 'blue';
         }
-        else{
-            butoon.style.backgroundColor = 'red'
-        }
+        console.log(savolObj.javob)
+        console.log(button.innerText)
     })
-});
+})
